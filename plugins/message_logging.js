bedd8d4c358bd7583b23a09e9cda033841db2630
ChilliesVo <97328523+chillivo@users.noreply.github.com>
2025-11-19 21:18:44 +0700

    fix log error handle

--- plugins/message_logging.js
+++ plugins/message_logging.js
@@ -55,13 +55,16 @@ exports.save_message_to_db = async function (next, connection, params) {
 exports.error_handle = async function (next, connection, params) {
   try {
     const { EmailTransaction } = server.notes.db
-    const harakaId = connection.transaction.uuid
+    const harakaId = connection.transaction?.uuid
+    if (!harakaId) next()
+
     const transaction = await EmailTransaction.findOne({ where: { harakaId } })
-    if (!transaction) throw new Error(`Not found any transaction by id ${harakaId}`)
+    if (!transaction) this.logerror(`Not found any transaction by id ${harakaId}`)
 
     transaction.status = EMAIL_STATUS.FAIL
     transaction.statusMessage = JSON.stringify(params)
     await transaction.save()
+
     next()
   } catch (err) {
     this.logerror(err)
