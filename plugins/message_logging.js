042b9b1aa46488b73df4e7d2198853725981059b
ChilliesVo <97328523+chillivo@users.noreply.github.com>
2025-11-20 09:36:00 +0700

    fix bug

--- plugins/message_logging.js
+++ plugins/message_logging.js
@@ -10,7 +10,7 @@ exports.register = function () {
 exports.request_message = async function (next, connection, params) {
   try {
     const harakaId = connection.transaction?.uuid
-    if (!harakaId) next()
+    if (!harakaId) return next()
     const { EmailAccount, EmailTransaction } = server.notes.db
     const accountRequest = connection.notes.auth_user
     if (!accountRequest) throw new Error(`Invalid Auth`)
@@ -19,7 +19,7 @@ exports.request_message = async function (next, connection, params) {
     const toAddresses = connection.transaction.rcpt_to.map((r) => r.address())
     for (const to of toAddresses) {
       await EmailTransaction.create({
-        harakaId: connection.transaction.uuid,
+        harakaId,
         userId: account.id,
         clientIP: connection.remote.ip,
         port: connection.local.port,
@@ -41,11 +41,11 @@ exports.save_message_to_db = async function (next, connection, params) {
   try {
     const { EmailTransaction } = server.notes.db
     const harakaId = connection.transaction?.uuid
-    if (!harakaId) next()
+    if (!harakaId) return next()
     const transactions = await EmailTransaction.findAll({ where: { harakaId } })
     if (!transactions.length) {
       this.logerror(`Not found any transaction by id ${harakaId}`)
-      next()
+      return next()
     }
     await EmailTransaction.update({ where: { harakaId } }, { status: EMAIL_STATUS.SUCCESS, statusMessage: 'Success' })
     next(OK)
@@ -58,11 +58,11 @@ exports.error_handle = async function (next, connection, params) {
   try {
     const { EmailTransaction } = server.notes.db
     const harakaId = connection.transaction?.uuid
-    if (!harakaId) next()
+    if (!harakaId) return next()
     const transactions = await EmailTransaction.findAll({ where: { harakaId } })
     if (!transactions.length) {
       this.logerror(`Not found any transaction by id ${harakaId}`)
-      next()
+      return next()
     }
     await EmailTransaction.update({ where: { harakaId } }, { status: EMAIL_STATUS.FAIL, statusMessage: JSON.stringify(params) })
     next()
