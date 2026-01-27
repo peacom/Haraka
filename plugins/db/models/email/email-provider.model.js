012a9e149b1518b57ecf5f30dea193cbb57fd5c4
ChilliesVo <97328523+chillivo@users.noreply.github.com>
2026-01-27 11:17:07 +0700

    update email-provider model

--- plugins/db/models/email/email-provider.model.js
+++ plugins/db/models/email/email-provider.model.js
@@ -8,9 +8,10 @@ class EmailProvider extends Model {
       {
         emailTransactionId: { type: DataTypes.STRING(64), primaryKey: true },
         providerEmailTransactionId: { type: DataTypes.STRING(64) },
+        host: { type: DataTypes.STRING(255) },
         providerName: { type: DataTypes.STRING(64) },
         providerStatus: { type: DataTypes.TINYINT },
-        providerStatusMessage: { type: DataTypes.STRING(255) },
+        providerStatusMessage: { type: DataTypes.TEXT },
         extraData: { type: DataTypes.TEXT },
         lastUpdated: { type: DataTypes.DATE }
       },
